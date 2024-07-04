37c7d99d556247336057ece70b05a84b291bdc2e
ErosAnaya1 <eros.anaya@unmsm.edu.pe>
2024-07-03 23:30:41 -0500

    Login_Home_2

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login.js';
 import Home from './pages/Home.js';
 import Catalogo from './pages/Catalogo.js';
@@ -9,19 +9,22 @@ import Layout from './components/Layout/Layout.jsx'
 
 function App() {
   return (
-<<<<<<< Updated upstream
-    <Header />
-=======
-
         <Routes>
-          <Route path="/catalogo" element={<Catalogo />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/product/:productId" element={<Detalle />} />
-          <Route path="/home" element={<Home />} />
+        <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
-        </Routes>
->>>>>>> Stashed changes
+          <Route path="/home" element={<Home />} />
+          <Route path="/product/:productId" element={<Detalle />} />
+          <Route path="/catalogo">
+            <Route index element={<Catalogo />} />
+            <Route path="smartphones" element={<Catalogo category={1} title='Smartphones'/>} />
+            <Route path="smartwatches" element={<Catalogo category={2} title='Smartwatches' />} />
+            <Route path="tablets" element={<Catalogo category={3} title='Tablets' />} />
+            <Route path="accesorios" element={<Catalogo category={4} title='Accesorios' />} />
+          </Route> 
+        </Route>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
   );
 }
-
 export default App;
