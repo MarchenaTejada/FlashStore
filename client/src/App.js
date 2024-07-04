82be547b47893b447e827f965e482bbeb16a85b6
MarcoLopezSalinas <marcolopez287@gmail.com>
2024-07-04 12:13:59 -0500

    A little correction

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 import Login from './pages/Login.js';
 import Home from './pages/Home.js';
 import Catalogo from './pages/Catalogo.js';
@@ -10,23 +10,23 @@ import Layout from './components/Layout/Layout.jsx'
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/product/:productId" element={<Detalle />} />
-        <Route path="/local" element={<Stores />} />
-        <Route path="/catalogo">
-          <Route index element={<Catalogo />} />
-          <Route path="smartphones" element={<Catalogo category={1} title='Smartphones' />} />
-          <Route path="smartwatches" element={<Catalogo category={2} title='Smartwatches' />} />
-          <Route path="tablets" element={<Catalogo category={3} title='Tablets' />} />
-          <Route path="accesorios" element={<Catalogo category={4} title='Accesorios' />} />
+        <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/local" element={<Stores />} />
+          <Route path="/product/:productId" element={<Detalle />} />
+          <Route path="/catalogo">
+            <Route index element={<Catalogo />} />
+            <Route path="smartphones" element={<Catalogo category={1} title='Smartphones'/>} />
+            <Route path="smartwatches" element={<Catalogo category={2} title='Smartwatches' />} />
+            <Route path="tablets" element={<Catalogo category={3} title='Tablets' />} />
+            <Route path="accesorios" element={<Catalogo category={4} title='Accesorios' />} />
+          </Route> 
         </Route>
-      </Route>
-      <Route path="*" element={<NotFound />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
   );
 }
 
