adb460379000b02e8cc5b2d4f1afc94a4c5bf589
Cristhian <cristhian.atuncar@unmsm.edu.pe>
2024-07-05 12:27:33 -0500

    Change cors

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://flashstoreweb.netlify.app',
   credentials: true
 }));
 
