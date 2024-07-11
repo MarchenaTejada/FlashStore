98562352fe1e430df6d32ef808ec55ac9dc3538f
MarcoLopezSalinas <marcolopez287@gmail.com>
2024-07-11 07:15:53 -0500

    commit

--- server/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const productosController = require('../controllers/productosController');
-
-const router = express.Router();
-
-router.get('/productos', productosController.handleObtenerProductos);
-router.get('/producto/:id', productosController.handleObtenerProductoEspecifico);
-
-module.exports = router;
